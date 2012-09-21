681d9a37441a0e225cd34e05ae6683da4e98eb88
Rafael Caricio <rafael.jacinto@gmail.com>
2012-09-21 17:37:47 -0300

    begining of the search view for the alpha version.

--- images_api/handlers/base.py
+++ images_api/handlers/base.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from json import dumps
+
 import tornado.web
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def _error(self, status, msg=None):
         self.set_status(status)
         self.finish()
+
+    def respond_with(self, data):
+        self.add_header('Content-Type', 'application/json')
+        self.write(dumps(data))
+        self.finish()
+
+
