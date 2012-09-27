369fd3ee0273f3464930bd2bbfa62310fe3545af
Guilherme Cirne <gcirne@gmail.com>
2012-09-27 11:24:50 -0300

    fix tests failing when performing async http fetch.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import urllib
+from tornado.ioloop import IOLoop
 from tornado.testing import AsyncHTTPTestCase
 
 from images_api.app import ImagesApplication
 
 
 class BaseImagesAPITestCase(AsyncHTTPTestCase):
+    
     def get_app(self):
         return ImagesApplication()
 
@@ -17,3 +19,6 @@ class BaseImagesAPITestCase(AsyncHTTPTestCase):
             url = "%s?%s" % (url, urllib.urlencode(querystring))
         self.http_client.fetch(url, self.stop)
         return self.wait()
+
+    def get_new_ioloop(self):
+        return IOLoop.instance()
