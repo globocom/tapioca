01fceed9c1a3b087235c2a56ef172f3b54689216
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-01 16:52:17 -0300

    move bahaviour to the alpha release.

--- images_api/handlers/base.py
+++ images_api/handlers/base.py
@@ -1,25 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from json import dumps
-
 import tornado.web
 
 
 class BaseHandler(tornado.web.RequestHandler):
-
-    def _error(self, status, msg=None):
-        self.set_status(status)
-        self.finish()
-
-    def respond_with(self, data):
-        self.set_header('Content-Type', 'application/json')
-        data = dumps(data)
-        callback_name = self.get_argument('callback', default=None)
-        if callback_name:
-            data = "%s(%s)" % (callback_name, data)
-        self.write(data)
-        self.finish()
 
     @property
     def config(self):
