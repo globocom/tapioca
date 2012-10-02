fcfeaebb45786afa763c3a5a1574681d3383d230
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-02 17:20:06 -0300

    Basic class to handle API behaviour.

--- images_api/handlers/base.py
+++ images_api/handlers/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import json
 import tornado.web
 
 
@@ -9,3 +10,76 @@ class BaseHandler(tornado.web.RequestHandler):
     @property
     def config(self):
         return self.application.config
+
+class ApiResourceHandler(BaseHandler):
+
+    def encode(self, data):
+        return json.dumps(data)
+
+    def decode(self, data):
+        return self.loads(data)
+
+    # Generic API HTTP Verbs
+
+    def get(self, *args):
+        """ return the collection or a model """
+        if self.is_get_collection(*args):
+            self.write(self.encode(self.get_collection(*args)))
+        else:
+            model = self.get_model(*args)
+            if model:
+                self.write(self.encode(model))
+            else:
+                raise tornado.web.HTTPError(404)
+
+    def post(self, *args):
+        """ create a model """
+        resp = self.create_model(self.decode(self.request.body), *args)
+        self.write(json.dumps(resp))
+
+    def put(self, *args):
+        """ update a model """
+        resp = self.update_model(self.decode(self.request.body), *args)
+        self.write(json.dumps(resp))
+
+    def delete(self, *args):
+        """ delete a model """
+        self.delete_model(*args)
+
+    # Extension points
+
+    def is_get_collection(self, *args):
+        """ return true if this get is for a collection """
+        return len(args) == 0
+
+    def create_model(self, model, *args):
+        """ create model and return a dictionary of updated attributes """
+        raise tornado.web.HTTPError(404)
+
+    def get_collection(self, *args):
+        """ return the collection """
+        raise tornado.web.HTTPError(404)
+
+    def get_model(self, *args):
+        """ return a model, return None to indicate not found """
+        raise tornado.web.HTTPError(404)
+
+    def update_model(self, model, *args):
+        """ update a model """
+        raise tornado.web.HTTPError(404)
+
+    def delete_model(self, *args):
+        """ delete a model """
+        raise tornado.web.HTTPError(404)
+
+    def loads(self, *args):
+        item = {}
+        for param_name in self.collection_fields:
+            item[param_name] = self.get_argument(param_name, None)
+        return item
+
+    @property
+    def collection_fields(self):
+        """ return a list of allowed parameters for this resource """
+        raise NotImplementedError
+
---
+++ tests/acceptance/test_base_api.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from tests.acceptance import BaseImagesAPITestCase
+
+
+class TestBaseApiHandler():
+    pass
