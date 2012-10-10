6c396c08a61c9d0341ba95f235c7a3646a239834
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-10 15:52:22 -0300

    rename files to a proper name after the name of the mixin have changed.

--- images_api/handlers/__init__.py
+++ images_api/handlers/__init__.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-from .base_handler_mixin import ExtractArgumentsMixin
+from .extract_arguments_mixin import ExtractArgumentsMixin
 from .healthcheck import HealthCheckHandler
 from .thumbor_url import JsonpEnabledThumborUrlHandler
