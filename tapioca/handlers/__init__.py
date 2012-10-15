792260c4fa81e0c7d625c7d39cd5ad0410db14ce
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-15 13:36:45 -0300

    remove useless files

--- tapioca/handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-from .extract_arguments_mixin import ExtractArgumentsMixin
-from .healthcheck import HealthCheckHandler
-from .thumbor_url import JsonpEnabledThumborUrlHandler
-from .images import ImagesResourceHandler
