37c3256b7f077e80f62b7b243fefe43d990935b9
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-15 17:56:09 -0300

    remove spaces

--- tests/support/async_http_client_mixin.py
+++ tests/support/async_http_client_mixin.py
@@ -6,22 +6,22 @@ import urllib
 
 
 class AsyncHTTPClientMixin(object):
-    
+
     def get(self, path, **querystring):
         url = self.get_url(path)
         if querystring:
             url = "%s?%s" % (url, urllib.urlencode(querystring))
         return self._fetch(url, 'GET')
-    
+
     def post(self, url, data):
         return self._fetch(url, 'POST', body=data)
-    
+
     def put(self, url, data):
         return self._fetch(url, 'PUT', body=data)
-    
+
     def delete(self, url):
         return self._fetch(url, 'DELETE')
-    
+
     def _fetch(self, url, method, **kwargs):
         self.http_client.fetch(url, self.stop, method=method, **kwargs)
         return self.wait()
