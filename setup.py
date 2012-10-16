80f171b717c41943bc202639de5f390e57c1c04f
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-16 18:09:41 -0300

    fix in package generation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from glob import glob
 from setuptools import setup
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 
 setup(
@@ -32,7 +32,10 @@ setup(
     packages = ['tapioca'],
     package_dir = {"tapioca": "tapioca"},
     include_package_data = True,
-    data_files = [('templates', glob('tapioca/templates/*'))],
+    data_files = [('templates', [
+        'tapioca/templates/base.html',
+        'tapioca/templates/tapioca/resource.html',
+    ])],
     install_requires=[
       "tornado>=2.4",
       "mimeparse>=0.1.3"
