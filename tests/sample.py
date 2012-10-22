872d770aa0fd92895693d3825f4c87eb26031f93
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-22 16:05:49 -0200

    simple project example

--- tests/sample.py
+++ tests/sample.py
@@ -6,7 +6,23 @@ from tornado.httpserver import HTTPServer
 
 from tapioca import TornadoRESTful, ResourceHandler
 
-from tests.support import ResourceWithDocumentation
+
+class ProjectsHandler(ResourceHandler):
+
+    def get_collection(self, callback, *args, **kwargs):
+        callback([])
+
+    def get_model(self, key, *args, **kwargs):
+        return {}
+
+    def update_model(self, key, *args, **kwargs):
+        pass
+
+
+class CommentsHandler(ResourceHandler):
+
+    def delete_model(self, key, *args, **kwargs):
+        pass
 
 
 if __name__ == '__main__':
@@ -14,7 +30,8 @@ if __name__ == '__main__':
 
     api = TornadoRESTful(
             version='', base_url='http://127.0.0.1:8000', discovery=True)
-    api.add_resource('comments', ResourceWithDocumentation)
+    api.add_resource('projects', ProjectsHandler)
+    api.add_resource('comments', CommentsHandler)
     application = tornado.web.Application(api.get_url_mapping())
 
     server = HTTPServer(application)
