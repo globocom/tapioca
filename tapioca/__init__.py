90d7edee7a38af357f4ab09109ee2715e49f7c2f
Rafael Caricio <rafael.jacinto@gmail.com>
2012-11-26 16:08:55 -0200

    just externing the exceptions

--- tapioca/__init__.py
+++ tapioca/__init__.py
@@ -1,4 +1,5 @@
 from tapioca.rest_api import TornadoRESTful, ResourceHandler, \
         ResourceDoesNotExist
 from tapioca.serializers import Encoder, JsonEncoder, JsonpEncoder, HtmlEncoder
-from tapioca.request import RequestSchema, validate, optional
+from tapioca.request import RequestSchema, validate, optional, ParamError, \
+        ParamRequiredError, InvalidParamError
